de38ce26ebc3fcf89f91c18f22d5acfb152abd61
Chris Atkins <chris.a@buildkite.com>
2025-08-17 21:59:57 +1000

    try and override the docs container to add classes

---
+++ frontend/.storybook/preview.tsx
@@ -0,0 +1,38 @@
+import type { Preview } from '@storybook/react-vite';
+import { DocsContainer } from '@storybook/addon-docs/blocks';
+import '../src/style.css';
+
+import { withThemeByClassName } from "@storybook/addon-themes";
+
+const lightClasses = 'light bg-gray-100';
+const darkClasses = 'dark bg-radial from-gray-800 via-gray-800 to-gray-900';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      container: (props: any) => {
+        let theme = props?.context.store.userGlobals.globals.theme || 'light';
+        let propsWithTheme = { ...props };
+        return (
+          <DocsContainer {...propsWithTheme} className={theme == 'light' ? lightClasses : darkClasses} />
+        );
+      }
+    }
+  },
+
+  decorators: [withThemeByClassName({
+    themes: {
+      light: lightClasses,
+      dark: darkClasses,
+    },
+    defaultTheme: 'light',
+  })]
+};
+
+export default preview;
